4bd545bb94bb1b213971b821aa3f44018be4a2b4
Techniack <22966516+Techniack@users.noreply.github.com>
2021-01-18 16:49:09 -0500

    Code structure/setup completed.

--- Bot.js
+++ Bot.js
@@ -1,40 +1,30 @@
 // require the needed modules
-const fs = require('fs');
 const Discord = require('discord.js');
 const { Menu } = require('discord.js-menu');
-const { prefix, token, version, acknowledgments, support, testToken} = require('./config.json');
+const { prefix, version, acknowledgments, support} = require('./config.json');
 const { token, testToken} = require('./tokens.json');
-
+const { TriggerWords } = require('./triggerwords.json');
 
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./Commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./Commands/${file}`);
-
-	client.commands.set(command.name, command);
-}
 
 client.once('ready', () => {
 	console.log('Bot Ready!');
 	console.log(`Logged in as ${client.user.tag}`);
 });
 
+client.on('message', message => {
+  if (message.author.bot) return;
 
-client.on('message', async message => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+	const string = message.content;
+	const terms = TriggerWords
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
-
-  if (command === `ping`) {
-    client.commands.get('ping').execute(message, args);
-}else if (command === `about`) {
-  //client.commands.get('about').execute(message, args);
-}else if (command === `help`) {
-client.commands.get('help').execute(message, args);
+	if(new RegExp(terms.join("|")).test(string) === true) {
+		const user = message.author;
+		message.channel.send(`${user}, Please check your DM's`);
+		message.author.send(`Hello ${user}, I hope you are doing OK. I noticed that some messages you sent talk about depression and/or suicide. I just wanted to let you know that there is help availible.`);
+		message.author.send("If you are in the US, Please call `1-800-273-8255` for assistance. If you are outside the US, please consult the guide below for more information:");
+		message.author.send(`https://www.opencounseling.com/suicide-hotlines`);
+		message.author.send("If you would like to speek with a human, Please type `s.human`");
 }});
 // login to Discord with your app's token
 client.login(token);
